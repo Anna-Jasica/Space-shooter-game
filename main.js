6d6f366b9dc6ba490604dd4fc065f3c6d56e2fd9
Kamil Tomsia <kamiltomsia@gmail.com>
2020-03-14 21:28:26 +0100

    implemented firing on click

--- main.js
+++ main.js
@@ -1,36 +1,64 @@
 const btn = document.getElementById("startButton");
 
 function buttonClick() {
-  btn.style.display = "none";
-  document.getElementById("main").style.cursor = 'url("spaceship.jpg"), auto';
+    btn.style.display = "none";
+    document.getElementById("main").style.cursor = 'url("spaceship.jpg"), auto';
 }
 
 btn.addEventListener("click", buttonClick);
 const state = {
-  bullets: [],
-  enemies: []
+    bullets: [],
+    enemies: []
 };
 
 function init() {
-  window.addEventListener("click", fire);
+    window.addEventListener("click", fire);
 }
 
 function fire(event) {
-  const bullet = document.createElement("div");
-  bullet.classList.add("bullet");
-  bullet.style.top = `${event.y}px`;
-  bullet.style.left = `${event.x}px`;
-  document.getElementById("space-shooter").appendChild(bullet);
-  console.log("pew pew pew");
+    const bullet = document.createElement("div");
+    bullet.classList.add("bullet");
+    bullet.style.top = `${event.y}px`;
+    bullet.style.left = `${event.x}px`;
+    document.getElementById("space-shooter").appendChild(bullet);
+    console.log("pew pew pew");
 }
 
 function update() {
-  console.log("LIVE");
+    console.log("LIVE");
 }
 
 function startGame() {
-  init();
-  // window.requestAnimationFrame();
+    init();
+    // window.requestAnimationFrame();
+    const bullet = document.createElement("div");
+    bullet.classList.add("bullet");
+    bullet.style.top = `${event.y}px`;
+    bullet.style.left = `${event.x}px`;
+    document.getElementById("main").appendChild(bullet);
+}
+
+function update() {
+    const bullets = document.getElementsByClassName("bullet");
+    for (bullet of bullets) {
+        bullet.style.left = `${+bullet.style.left.slice(0, -2) + 1}px`;
+        if (!isBulletWithinScreen(bullet)) {
+            bullet.remove();
+        }
+    }
+    window.requestAnimationFrame(update);
+}
+
+function isBulletWithinScreen(bullet) {
+    if (+bullet.style.left.slice(0, -2) > window.innerWidth) {
+        return false;
+    }
+    return true;
+}
+
+function startGame() {
+    init();
+    update();
 }
 
 startGame();
@@ -42,3 +70,13 @@ console.log("TEST");
 //         this.y = y;
 //     }
 // }
+
+class Bullet {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        state.bullets.push;
+    }
+
+    draw() {}
+}
