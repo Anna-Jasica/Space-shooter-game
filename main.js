be9c92ce5b229f297c09184cd8e7df8d2f8ddcf9
Kamil Tomsia <kamtom@fideltronik.int>
2020-03-14 20:15:41 +0100

    added background color, started working on firing

---
+++ main.js
@@ -0,0 +1,36 @@
+const state = {
+    bullets: [],
+    enemies: []
+};
+
+function init() {
+    window.addEventListener("click", fire);
+}
+
+function fire(event) {
+    const bullet = document.createElement("div");
+    bullet.classList.add("bullet");
+    bullet.style.top = `${event.y}px`;
+    bullet.style.left = `${event.x}px`;
+    document.getElementById("space-shooter").appendChild(bullet);
+    console.log("pew pew pew");
+}
+
+function update() {
+    console.log("LIVE");
+}
+
+function startGame() {
+    init();
+    // window.requestAnimationFrame();
+}
+
+startGame();
+console.log("TEST");
+
+// class Coordinates {
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
