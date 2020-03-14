e30fa101efba86808174af6ada363b4cf4d911e4
Kamil Tomsia <kamiltomsia@gmail.com>
2020-03-14 23:19:07 +0100

    added enemies

--- main.js
+++ main.js
@@ -20,6 +20,31 @@ function fire(event) {
 }
 
 function update() {
+    handleEnemies();
+    handleBullets();
+    window.requestAnimationFrame(update);
+}
+
+function handleEnemies() {
+    const enemies = document.getElementsByClassName("enemy");
+    for (enemy of enemies) {
+        enemy.style.left = `${+enemy.style.left.slice(0, -2) - 1}px`;
+        // if (!isBulletWithinScreen(enemy)) {
+        //     enemy.remove();
+        // }
+    }
+}
+
+function spawnEnemy() {
+    const enemy = document.createElement("div");
+    enemy.classList.add("enemy");
+    enemy.style.top = `${getRandomInteger(0, window.innerHeight - 25)}px`;
+    enemy.style.left = `${window.innerWidth - 25}px`;
+    document.getElementById("main").appendChild(enemy);
+    console.log("am am am");
+}
+
+function handleBullets() {
     const bullets = document.getElementsByClassName("bullet");
     for (bullet of bullets) {
         bullet.style.left = `${+bullet.style.left.slice(0, -2) + 1}px`;
@@ -27,7 +52,6 @@ function update() {
             bullet.remove();
         }
     }
-    window.requestAnimationFrame(update);
 }
 
 function isBulletWithinScreen(bullet) {
@@ -40,9 +64,14 @@ function isBulletWithinScreen(bullet) {
 function startGame() {
     init();
     update();
+    setInterval(spawnEnemy, 1000);
 }
 
 startGame();
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 class Bullet {
     constructor(x, y) {
