22f50fc0aa550d98e95236db6450342fb6595157
Ania Jasica <ania.jasica@gmail.com>
2020-04-24 13:24:07 +0200

    ship undraggable, delete lodash

--- src/index.js
+++ src/index.js
@@ -35,7 +35,6 @@ window.init = function (event) {
     document.getElementById("endButton").style.display = "none";
     document.getElementById("gameOver").style.display = "none";
     document.getElementById("ship").style.display = "block";
-    document.getElementById("ship").setAttribute("draggable", false);
     document.getElementById("main").style.cursor = "none";
     window.addEventListener("click", fire, true);
     window.addEventListener("mousemove", shipTrack);
@@ -46,10 +45,10 @@ function fire(event) {
     const bullet = document.createElement("div");
     const ship = document.getElementById("ship");
     bullet.classList.add("bullet");
+    bullet.classList.add("undraggable");
     document.getElementById("main").appendChild(bullet);
     bullet.style.top = `${event.y}px`;
     bullet.style.left = `${event.x + getWidth(ship) / 2}px`;
-    bullet.setAttribute("draggable", false);
 }
 
 function shipTrack(e) {
